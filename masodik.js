cdfaf98c7c75c857bf009286e951a5cda627c740
Kovács Bence <hobbemail0@gmail.com>
2023-09-27 10:32:00 +0200

    Lekérdezés 2. feladat

---
+++ masodik.js
@@ -0,0 +1,12 @@
+const lista = [];
+
+document.addEventListener("DOMContentLoaded", () => ){
+    const urlap = document.forms("urlap")
+    urlap.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const nev = urlap["urlap"]["nev"];
+
+    })
+    
+};
+
