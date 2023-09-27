cb88e66715243a02195d7430722f52d596566064
Kovács Bence <hobbemail0@gmail.com>
2023-09-27 17:50:02 +0200

    feladat 1 javítva.

--- elso.js
+++ elso.js
@@ -1,26 +1,32 @@
 let betumeret = "nincs";
+let atalakitas = true;
 function kisBetu(){
     betumeret = document.getElementById("kisBetu").value;
-    // console.log(betumeret);
+    console.log(betumeret);
 }
 function nagyBetu(){
     betumeret = document.getElementById("nagyBetu").value;
-    // console.log(betumeret);
+    console.log(betumeret);
 
 }
 function varazsoljunk(){
-    let atalakitas = true;
+    
     szoveg = document.getElementById("szoveg").value;
     // console.log(szoveg)
     if (betumeret == "kisbetu"){
         szoveg = szoveg.toLowerCase();
+        atalakitas = true;
     } else if (betumeret == "nagyBetu"){
         szoveg = szoveg.toUpperCase();
+        atalakitas = true;
     } else if (betumeret == "nincs"){
         alert("Nem választottad ki, hogy kis vagy nagybetűre történjen az átalakítás.")
+        atalakitas = false;
+    }
+    if (szoveg == ""){
+        alert("Kérlek add meg az átalakítandó szöveget")
+        atalakitas = false;
     }
     // console.log(szoveg);
-    if (atalakitas){
     document.getElementById("szoveg").value = szoveg;
-    }
 }
