4fd151715d940a6481dd87b3ff7b7ea12fdee5d5
Kovács Bence <hobbemail0@gmail.com>
2023-09-27 12:33:46 +0200

    masodik feladat kész

--- masodik.js
+++ masodik.js
@@ -1,12 +1,41 @@
 const lista = [];
 
-document.addEventListener("DOMContentLoaded", () => ){
-    const urlap = document.forms("urlap")
+document.addEventListener("DOMContentLoaded", () => {
+    const urlap = document.forms["urlap"]
     urlap.addEventListener("submit", (event) => {
         event.preventDefault();
-        const nev = urlap["urlap"]["nev"];
+        const nev = urlap["nev"].value;
+        const email = urlap["email"].value;
+        const jelszo = urlap["jelszo"].value;
+        const jelszoRe = urlap["jelszoRe"].value;
+        const datum = urlap["datum"];
+        nevhossz = nev.lenght;
+        console.log(nev, email, jelszo, jelszoRe, nevhossz);
+        let megfelelo = true;
+        if (nev.lenght <= 4 || nev.lenght >= 17){
+            alert("A név hossza 5 és 16 betű között legyen!")
+            megfelelo = false;
+        }
+        if (jelszo != jelszoRe){
+            alert("A jelszavaknak meg kell egyezniük!")
+            megfelelo = false;
+        }
+        if (jelszo.lenght <= 4){
+            alert("A jelszó minimum 5 karakter kell, hogy legyen!")
+            megfelelo = false;
+        }
+        if(megfelelo){
+            const adatok ={
+                nev: nev,
+                email: email,
+                jelszo: jelszo,
+                datum: datum,
+            }
+            lista.push(adatok);
+            console.log(lista);
+        }
 
     })
     
-};
+});
 
